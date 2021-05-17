be0173d00acca8ae2ceb0e4192b3abd95a629b39
Martin Tarjanyi <mtarjanyi@qatarairways.com.qa>
2021-05-17 08:48:34 +0200

    Improve property handling and add jackson time handling

--- src/main/kotlin/org/example/HttpClient.kt
+++ src/main/kotlin/org/example/HttpClient.kt
@@ -2,6 +2,7 @@ package org.example
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -13,8 +14,10 @@ fun createClient(customize: HttpClientConfig<CIOEngineConfig>.() -> Unit = {}) =
             serializer = JacksonSerializer(
                 ObjectMapper()
                     .registerModule(KotlinModule(nullIsSameAsDefault = true))
+                    .registerModule(JavaTimeModule())
                     .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
                     .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                    .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
             )
         }
         customize()
--- src/main/kotlin/org/example/Utils.kt
+++ src/main/kotlin/org/example/Utils.kt
@@ -1,4 +1,10 @@
 package org.example
 
-fun property(propertyName: String, default: String? = null) =
-    requireNotNull(System.getProperty(propertyName, default)) { "$propertyName property is mandatory" }
+fun property(propertyName: String): String =
+    requireNotNull(System.getProperty(propertyName)) { "$propertyName property is mandatory" }
+
+fun optionalProperty(propertyName: String, default: String): String =
+    System.getProperty(propertyName, default)
+
+fun optionalProperty(propertyName: String): String? =
+    System.getProperty(propertyName)
